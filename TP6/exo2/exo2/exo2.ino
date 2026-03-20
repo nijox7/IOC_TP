6043392b164ee094a088f2befaf81eee45e806ec
Clément Carbou <113636309+nijox7@users.noreply.github.com>
2026-03-20 12:47:27 +0100

    TP6 fac

---
+++ TP6/exo2/exo2/exo2.ino
@@ -0,0 +1,128 @@
+// --------------------------------------------------------------------------------------------------------------------
+// Multi-tâches cooperatives : solution basique mais efficace :-)
+// --------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------
+// unsigned int waitFor(timer, period) 
+// Timer pour taches périodiques 
+// configuration :
+//  - MAX_WAIT_FOR_TIMER : nombre maximum de timers utilisés
+// arguments :
+//  - timer  : numéro de timer entre 0 et MAX_WAIT_FOR_TIMER-1
+//  - period : période souhaitée
+// retour :
+//  - nombre de périodes écoulées depuis le dernier appel
+// --------------------------------------------------------------------------------------------------------------------
+
+#define MAX_WAIT_FOR_TIMER 5
+unsigned long waitFor(int timer, unsigned long period) {
+    static unsigned long last_period[MAX_WAIT_FOR_TIMER];  // il y a autant de timers que de tâches
+    unsigned long current = micros() / period;             // numéro de période
+    unsigned long delta   = current - last_period[timer];  // gère le wrap-around
+    if (delta) last_period[timer] = current;               // mise à jour si déclenchement
+    return delta;                                          // nombre de periode depuis le dernier appel
+}
+
+//--------- définition de la tache Led
+
+struct ctx_led_t {
+  int timer;                                              // n° du timer pour cette tâche utilisé par WaitFor
+  unsigned long period;                                   // periode de clignotement
+  int pin;                                                // numéro de la broche sur laquelle est la LED
+  int etat;                                               // etat interne de la led
+};
+
+void init_led(struct ctx_led_t * ctx, int timer, unsigned long period, byte pin) {
+  ctx->timer = timer;
+  ctx->period = period;
+  ctx->pin = pin;
+  ctx->etat = 0;
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin, ctx->etat);
+}
+
+void step_led(struct ctx_led_t * ctx) {
+  if (!waitFor(ctx->timer, ctx->period)) return;          // sort s'il y a moins d'une période écoulée
+  digitalWrite(ctx->pin,ctx->etat);                       // ecriture
+  ctx->etat = 1 - ctx->etat;                              // changement d'état
+}
+
+//--------- definition de la tache Mess
+
+struct ctx_mess_t {
+  int timer;                                              // numéro de timer utilisé par WaitFor
+  unsigned long period;                                   // periode d'affichage
+  char mess[20];
+};
+
+void init_mess(struct ctx_mess_t* ctx, int timer, unsigned long period, const char * mess) {
+  ctx->timer = timer;
+  ctx->period = period;
+  strcpy(ctx->mess, mess);
+  Serial.begin(9600);                                     // initialisation du débit de la liaison série
+}
+
+void step_mess(struct ctx_mess_t *ctx) {
+  if (!(waitFor(ctx->timer,ctx->period))) return;         // sort s'il y a moins d'une période écoulée
+  Serial.println(ctx->mess);                              // affichage du message
+}
+
+//--------- definition de la tache Oled
+
+struct ctx_oled_t {
+  int timer;
+  unsigned long period;
+  int pin;
+  char mess[20];
+}
+
+void init_oled(struct ctx_oled_t* ctx, int timer, unsigned long period, const char* mess){
+  ctx->timer = timer;
+  ctx->period = period;
+  strcpy(ctx->mess, mess);
+  // TODO OLED INIT
+}
+
+void step_oled(){
+  
+}
+
+//--------- definition de la tache  lum
+
+enum {EMPTY, FULL};
+
+struct mailbox_t {
+  int state;
+  int val;
+};
+
+struct mailbox_t mb = {.state = EMPTY};
+
+void init_lum(){
+  // TODO
+}
+
+void step_lum(mailbox_t * mb){
+  int lum = analogRead(); // TODO UTILISER MAP
+}
+
+//--------- Déclaration des contexte de tâches
+
+struct ctx_led_t Led1;
+struct ctx_mess_t Mess1;
+struct ctx_mess_t Mess2;
+struct ctx_oled_t Oled;
+
+//--------- Setup et Loop
+
+void setup() {
+  init_led(&Led1, 0, 100000, LED_BUILTIN);               // Led est exécutée toutes les 100ms 
+  init_mess(&Mess1, 1, 1000000, "bonjour");              // Mess est exécutée toutes les secondes 
+  init_mess(&Mess2, 2, 2000000, "Salut");
+}
+
+void loop() {
+  step_led(&Led1);                                        
+  step_mess(&Mess1);
+  step_mess(&Mess2);
+}
--- TP6/ioc26_6_carbou_das.md
+++ TP6/ioc26_6_carbou_das.md
@@ -7,7 +7,9 @@
 ## Pour lancer arduino
 Aller dans une. session avec l'os Mate (Fermer la session -> login -> en bas a droite selectionner mate -> mot de passe)
 /opt/arduino-1.8.8/arduino
-
+    Dans le texte précédent, quel est le nom de la boîte à lettre et comment est-elle initialisée ?
+    Ajouter une tâche nommée lum qui lit toutes les 0,5 seconde le port analogique [...] (par analogRead()) sur lequel se trouve la photo-résistance et qui sort sa valeur dans une boite à lettre. Cette boite à lettre sera connectée à la tâche oled. Vous afficher la valeur en pourcentage de 0% à 100% en utilisant la fonction map()
+    Mofifier la tâche Led pour que la fréquence de clignotement soit inversement proportionnel à la lumière reçue (moins il y a de lumière plus elle clignote vite). La tâche Led devra donc se brancher avec la tâche lum avec une nouvelle boite à lettre. Il doit y avoir deux boites sortant de lum, l'une vers oled l'autre vers led. 
 ## TODO
  Vous rendrez une archive avec :
     un compte rendu au format en pdf (à partir de Markdown ou d'un traitement de texte) expliquant en détail votre sketch Arduino, l'idée est de vous approprier cette méthode de programmation, alors les explications que vous faites sont utiles pour vous. Vous devez aussi insérer un graphe représentant les tâches et leur communication (avec graphviz).
@@ -21,15 +23,28 @@ La première est que si l'on déclare une variable static dans init_tache, elle 
 La seconde est que l'utilisation d'un variable static dans step_tache, rend cette variable commune à tous les taches.
 
 - Que contient le tableau last_period[] et à quoi sert-il ?
+
 Le tableau last_period[] contient le numéro de la dernière période lors du dernier appel de waitFor pour chaque timer.
 
 - Si on a deux tâches indépendantes avec la même période, pourquoi ne peut-on pas utiliser le même timer dans waitFor() ?
+
 On ne peut pas utiliser le même timer pour deux tâches indépendantes car lors de l'appel waitFor, la valeur dans last_period[timer] est mise à jour.
 Seule la première des taches à faire l'appel à waitFor obtiendra le nombre de période écoulée, l'autre obtiendra 0 puisque le tableau static last_period aura été modifié.
 
 - Dans quel cas la fonction waitFor() peut rendre 2 ?
+
 La fonction waitFor() peut rendre 2 lorsque au moins 2 périodes se sont écoulées.
 
+
+
+- Dans le texte précédent, quel est le nom de la boîte à lettre et comment est-elle initialisée ?
+
+Le nom de la boîte aux lettre est mb. Elle est initialisée à l'aide d'une structure possèdant l'état de la boîte et sa valeur. Elle est initialement à l'état vide et n'a donc pas de valeur.
+
+- Ajouter une tâche nommée lum qui lit toutes les 0,5 seconde le port analogique [...] (par analogRead()) sur lequel se trouve la photo-résistance et qui sort sa valeur dans une boite à lettre. Cette boite à lettre sera connectée à la tâche oled. Vous afficher la valeur en pourcentage de 0% à 100% en utilisant la fonction map()
+
+
+- Modifier la tâche Led pour que la fréquence de clignotement soit inversement proportionnel à la lumière reçue (moins il y a de lumière plus elle clignote vite). La tâche Led devra donc se brancher avec la tâche lum avec une nouvelle boite à lettre. Il doit y avoir deux boites sortant de lum, l'une vers oled l'autre vers led. 
 
 
 ## Expériences réalisées
